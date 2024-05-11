8495c1784eb4a67eb7844c6c6a3c361e798600de
guimiiller <guilhermemillerblack@gmail.com>
2024-05-11 09:58:55 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+
+/* MENU */
+const menu = document.querySelector('.menu')
+const navMenu = document.querySelector('.nav-menu')
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('ativo')
+    navMenu.classList.toggle('ativo')
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var menuLinks = document.querySelectorAll('.nav-menu a');
+
+    
+    menuLinks.forEach(function(menuLink) {
+        menuLink.addEventListener('click', function() {
+            
+            document.querySelector('.menu').classList.remove('ativo');
+            document.querySelector('.nav-menu').classList.remove('ativo');
+        });
+    });
+});
+
+
+/* BORDA */
+document.addEventListener("DOMContentLoaded", function() {
+    const navItems = document.querySelectorAll(".nav-item");
+
+    // Adiciona um evento de clique a cada item de navegação
+    navItems.forEach(function(item) {
+        item.addEventListener("click", function(event) {
+            
+            navItems.forEach(function(navItem) {
+                navItem.classList.remove("active");
+            });
+
+            
+            item.classList.add("active");
+        });
+    });
+});
+/* BORDA */
+
+
+/* SCROLL */
+const target  = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element){
+        if ((windowTop) > element.offsetTop){
+            element.classList.add(animationClass)
+        }else {
+            element.classList.remove(animationClass)
+        }
+    })
+}
+
+animeScroll()
+
+window.addEventListener('scroll', function(){
+    animeScroll()
+})
+/* SCROLL */
